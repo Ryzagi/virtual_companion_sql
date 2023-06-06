bb20f8d4933c98c7e9c826b93b3fac39a492cfe4
Ryzagi <vladislavrzhv@gmail.com>
2023-06-06 18:41:21 +0300

    fix: minor fixings

--- converbot/database/conversations.py
+++ converbot/database/conversations.py
@@ -99,7 +99,7 @@ class ConversationDB:
                 """
                 SELECT * FROM Companions WHERE user_id = %(user_id)s
                 """,
-                {'user_id': int(user_id)}
+                {'user_id': str(user_id)}
             )
 
             # Fetch all the rows returned by the query
